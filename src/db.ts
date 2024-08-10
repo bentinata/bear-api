b6504bc254eeed90f1e70d758866d6e350e3ad42
Ben Nata <_@bentinata.com>
2024-08-10 22:35:50 +0700

    feat: Add POSTGRES_HOST env var to be used inside compose

--- src/db.ts
+++ src/db.ts
@@ -3,10 +3,13 @@ import postgres from "postgres";
 import "./env";
 import * as schema from "./schema";
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
+const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
+  process.env;
 
 export const connection = postgres(
-  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@0.0.0.0:5432/${POSTGRES_DB}`
+  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${
+    POSTGRES_HOST || "0.0.0.0"
+  }:5432/${POSTGRES_DB}`
 );
 
 export const db = drizzle(connection, { schema });
--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,6 @@
 declare module "bun" {
   interface Env {
+    POSTGRES_HOST: string;
     POSTGRES_USER: string;
     POSTGRES_PASSWORD: string;
     POSTGRES_DB: string;

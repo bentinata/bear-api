bc01db779b34837975285610b9dabe3d4a903817
Ben Nata <_@bentinata.com>
2024-08-10 22:37:54 +0700

    chore(db): Add drizzle config
    To use, run:
    bunx drizzle-kit push

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import "./src/env";
+
+export default defineConfig({
+  schema: "./src/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    host: process.env.POSTGRES_HOST || "0.0.0.0",
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+});
